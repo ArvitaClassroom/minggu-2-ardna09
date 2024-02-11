bc66f22744f4c2de4baa85a6d8880dc1f7b12d3a
Ardana09 <158832887+ardna09@users.noreply.github.com>
2024-02-11 17:54:45 +0700

    Update variable.dart

--- variable.dart
+++ variable.dart
@@ -5,19 +5,13 @@ void main() {
   print(name); // "ardana"
   print(angka); // 09
   print(todayIsFriday); // false
-}
 
-void main() {
   var number = 19;
   print(number < 22); // true
   print(number > 19); // false
   print(number >= 19); // true, karena terdapat sama dengan
   print(number <= 22); // true
-}
-void main() {
-  var number = 19;
-  print(number 10+9); // true
-  print(number 3*9); // false
-  print(number 361/19); // true
-  print(number 27-8); // true
+
+  print(number = 10 + 9); // true
+  print(number = 27 - 8); // true
 }
