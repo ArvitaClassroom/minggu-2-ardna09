0d7eb0f2991f7b8c5e27f3b6b357db58aa4e7991
Ardana09 <158832887+ardna09@users.noreply.github.com>
2024-02-11 17:53:13 +0700

    Update For-loop.dart

--- For-loop.dart
+++ For-loop.dart
@@ -1,8 +1,15 @@
-void main() {
+// ignore_for_file: for_loop
+
+Future<void> main() async {
   var jumlah = 0;
   for (var deret = 5; deret > 0; deret--) {
     jumlah += deret;
-    print('Jumlah saat ini: ' + jumlah.toString());
+    print(newMethod(jumlah));
   }
-  print('Jumlah terakhir: ' + jumlah.toString());
+  var object = 'Jumlah terakhir: $jumlah';
+  print(ardana(object));
 }
+
+String ardana(String object) => object;
+
+String newMethod(int jumlah) => 'Jumlah saat ini: $jumlah';
